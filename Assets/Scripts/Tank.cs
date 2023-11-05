ecc188274949b07a78d59541cc3ef1f7e87732eb
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-11-05 14:46:15 +0200

    tank movement

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+
+    public string verticalAxis;
+    public string horizontalAxis;
+
+    void Update()
+    {
+        var ver = Input.GetAxis(verticalAxis);
+        transform.position += transform.forward * speed * ver * Time.deltaTime;
+
+        var hor = Input.GetAxis(horizontalAxis);
+        transform.Rotate(0,rotateSpeed * hor * Time.deltaTime,0);
+    }
+}
